72c6dc5ad529e1ad1df0d4a690cd286bef6c94d2
melinte-alexandru <alexandru15042004@gmail.com>
2026-07-16 00:18:12 +0300

    Rezolvat exercitiul 5 Set 3

---
+++ Set3/ex5.py
@@ -0,0 +1,6 @@
+# 5. Validare dicționar
+def validate_dict(rules, data):
+    for key, val in data.items():
+        # logică validare prefix, middle, suffix
+        pass # Implementare bazată pe regex sau startswith/endswith
+    return True
