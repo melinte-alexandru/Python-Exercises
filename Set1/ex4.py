c386733b1ae5b7db0b74ffaa94872be5812a8ff6
melinte-alexandru <alexandru15042004@gmail.com>
2026-07-16 00:14:06 +0300

    Rezolvat exercitiul 4 Set 1

---
+++ Set1/ex4.py
@@ -0,0 +1,4 @@
+# 4. Write a function that receives two strings as parameters and returns the number of occurrences of the first string in the second
+def count_occurrences(sub, s):
+    """Numără aparițiile primului string în al doilea."""
+    return s.count(sub)
