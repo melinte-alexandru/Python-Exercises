5f0fb84e8f807868839b05afaf7efa94b545b8f4
melinte-alexandru <alexandru15042004@gmail.com>
2026-07-16 00:16:19 +0300

    Rezolvat exercitiul 2 Set 2

---
+++ Set2/ex2.py
@@ -0,0 +1,8 @@
+# 2. Primele dintr-o listă
+def get_primes(numbers):
+    def is_prime(num):
+        if num < 2: return False
+        for i in range(2, int(num**0.5) + 1):
+            if num % i == 0: return False
+        return True
+    return [n for n in numbers if is_prime(n)]
